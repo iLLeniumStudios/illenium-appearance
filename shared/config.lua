67ae74fa9822704ca278daf4f3a4e7592f3d87dd
G-alambb <105150946+G-alambb@users.noreply.github.com>
2022-05-14 22:13:27 +0200

    Config for peds

--- shared/config.lua
+++ shared/config.lua
@@ -2,6 +2,8 @@ Config = {}
 
 Config.Debug = false
 
+Config.ped = true
+
 Config.ClothingCost = 100
 Config.BarberCost = 100
 Config.TattooCost = 100
@@ -46,36 +48,37 @@ Config.Blips = {
 }
 
 Config.Stores = {
-    [1] = {shopType = 'clothing', coords = vector3(1693.2, 4828.11, 42.07), width = 4, length = 4, showBlip = true}, -- showBlip overrides the blip visibilty configured above for the group
-    [2] = {shopType = 'clothing', coords = vector3(-705.5, -149.22, 37.42), width = 4, length = 4},
-    [3] = {shopType = 'clothing', coords = vector3(-1192.61, -768.4, 17.32), width = 4, length = 4},
-    [4] = {shopType = 'clothing', coords = vector3(425.91, -801.03, 29.49), width = 4, length = 4},
-    [5] = {shopType = 'clothing', coords = vector3(-168.73, -301.41, 39.73), width = 4, length = 4},
-    [6] = {shopType = 'clothing', coords = vector3(75.39, -1398.28, 29.38), width = 4, length = 4},
-    [7] = {shopType = 'clothing', coords = vector3(-827.39, -1075.93, 11.33), width = 4, length = 4},
-    [8] = {shopType = 'clothing', coords = vector3(-1445.86, -240.78, 49.82), width = 4, length = 4},
-    [9] = {shopType = 'clothing', coords = vector3(9.22, 6515.74, 31.88), width = 4, length = 4},
-    [10] = {shopType = 'clothing', coords = vector3(615.35, 2762.72, 42.09), width = 4, length = 4},
-    [11] = {shopType = 'clothing', coords = vector3(1191.61, 2710.91, 38.22), width = 4, length = 4},
-    [12] = {shopType = 'clothing', coords = vector3(-3171.32, 1043.56, 20.86), width = 4, length = 4},
-    [13] = {shopType = 'clothing', coords = vector3(-1105.52, 2707.79, 19.11), width = 4, length = 4},
-    [14] = {shopType = 'clothing', coords = vector3(-1207.65, -1456.89, 4.37), width = 4, length = 4},
-    [15] = {shopType = 'clothing', coords = vector3(124.82, -224.36, 54.56), width = 2, length = 2},
-    [16] = {shopType = 'barber', coords = vector3(-814.3, -183.8, 36.6), width = 2, length = 2},
-    [17] = {shopType = 'barber', coords = vector3(136.8, -1708.4, 28.3), width = 2, length = 2},
-    [18] = {shopType = 'barber', coords = vector3(-1282.6, -1116.8, 6.0), width = 2, length = 2},
-    [19] = {shopType = 'barber', coords = vector3(1931.5, 3729.7, 31.8), width = 2, length = 2},
-    [20] = {shopType = 'barber', coords = vector3(1212.8, -472.9, 65.2), width = 2, length = 2},
-    [21] = {shopType = 'barber', coords = vector3(-32.9, -152.3, 56.1), width = 2, length = 2},
-    [22] = {shopType = 'barber', coords = vector3(-278.1, 6228.5, 30.7), width = 2, length = 2},
-    [23] = {shopType = 'tattoo', coords = vector3(1322.6, -1651.9, 51.2), width = 2, length = 2},
-    [24] = {shopType = 'tattoo', coords = vector3(-1153.6, -1425.6, 4.9), width = 2, length = 2},
-    [25] = {shopType = 'tattoo', coords = vector3(322.1, 180.4, 103.5), width = 2, length = 2},
-    [26] = {shopType = 'tattoo', coords = vector3(-3170.0, 1075.0, 20.8), width = 2, length = 2},
-    [27] = {shopType = 'tattoo', coords = vector3(1864.6, 3747.7, 33.0), width = 2, length = 2},
-    [28] = {shopType = 'tattoo', coords = vector3(-293.7, 6200.0, 31.4), width = 2, length = 2},
-    [29] = {shopType = 'surgeon', coords = vector3(299.68, -572.99, 43.26), width = 2, length = 2},
+    [1] = {shopType = 'clothing', coords = vector4(1693.92, 4830.82, 42.07, 240), width = 4, length = 4, showBlip = true}, -- showBlip overrides the blip visibilty configured above for the group
+    [2] = {shopType = 'clothing', coords = vector4(-713.13, -153.44, 36.95, 250), width = 4, length = 4},
+    [3] = {shopType = 'clothing', coords = vector4(-1192.25, -772.26, 18.54,250), width = 4, length = 5},
+    [4] = {shopType = 'clothing', coords = vector4(425.91, -801.03, 29.49,250), width = 4, length = 4},
+    [5] = {shopType = 'clothing', coords = vector4(-161.02, -305.14, 41.26,250), width = 4, length = 4},
+    [6] = {shopType = 'clothing', coords = vector4(74.35, -1400.78, 29.38,250), width = 4, length = 4},
+    [7] = {shopType = 'clothing', coords = vector4(-827.39, -1075.93, 11.33,250), width = 4, length = 4},
+    [8] = {shopType = 'clothing', coords = vector4(-1452.52, -234.81, 49.8,250), width = 4, length = 4},
+    [9] = {shopType = 'clothing', coords = vector4(11.0, 6517.29, 31.88,250), width = 4, length = 4},
+    [10] = {shopType = 'clothing', coords = vector4(617.65, 2761.19, 42.09,250), width = 4, length = 4},
+    [11] = {shopType = 'clothing', coords = vector4(1188.76, 2710.7, 38.22,250), width = 4, length = 4},
+    [12] = {shopType = 'clothing', coords = vector4(-3171.32, 1043.56, 20.86,250), width = 4, length = 4},
+    [13] = {shopType = 'clothing', coords = vector4(-1105.52, 2707.79, 19.11,250), width = 4, length = 4},
+    [14] = {shopType = 'clothing', coords = vector4(-1207.65, -1456.89, 4.37,250), width = 4, length = 4},
+    [15] = {shopType = 'clothing', coords = vector4(124.82, -224.36, 54.56,250), width = 2, length = 2},
+    [16] = {shopType = 'barber', coords = vector4(-814.3, -183.8, 36.6,250), width = 2, length = 2},
+    [17] = {shopType = 'barber', coords = vector4(136.8, -1708.4, 28.3,250), width = 2, length = 2},
+    [18] = {shopType = 'barber', coords = vector4(-1282.6, -1116.8, 6.0,250), width = 2, length = 2},
+    [19] = {shopType = 'barber', coords = vector4(1931.5, 3729.7, 31.8,250), width = 2, length = 2},
+    [20] = {shopType = 'barber', coords = vector4(1212.8, -472.9, 65.2,250), width = 2, length = 2},
+    [21] = {shopType = 'barber', coords = vector4(-32.9, -152.3, 56.1,250), width = 2, length = 2},
+    [22] = {shopType = 'barber', coords = vector4(-278.1, 6228.5, 30.7,250), width = 2, length = 2},
+    [23] = {shopType = 'tattoo', coords = vector4(1322.6, -1651.9, 51.2,250), width = 2, length = 2},
+    [24] = {shopType = 'tattoo', coords = vector4(-1153.6, -1425.6, 4.9,250), width = 2, length = 2},
+    [25] = {shopType = 'tattoo', coords = vector4(322.1, 180.4, 103.5,250), width = 2, length = 2},
+    [26] = {shopType = 'tattoo', coords = vector4(-3170.0, 1075.0, 20.8,250), width = 2, length = 2},
+    [27] = {shopType = 'tattoo', coords = vector4(1864.6, 3747.7, 33.0,250), width = 2, length = 2},
+    [28] = {shopType = 'tattoo', coords = vector4(-293.7, 6200.0, 31.4,250), width = 2, length = 2},
+    [29] = {shopType = 'surgeon', coords = vector4(311.33, -570.48, 42.28,250),width = 2, length = 2},
 }
+
 
 Config.ClothingRooms = {
     [1] = {requiredJob = "police", isGang = false, coords = vector3(463.43, -999.07, 30.69), width = 3, length = 3},
