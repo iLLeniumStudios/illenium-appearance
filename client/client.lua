03a3459fcfa5a0b74d3db1553494db6d61442231
iLLeniumStudios <104288623+TheiLLeniumStudios@users.noreply.github.com>
2022-07-02 16:55:17 +0200

    feat(client): Allow disabling components / props entirely (#121)
    * feat(client): Allow disabling components / props entirely
    
    * fix(client): Change is-clothing to fivem-appearance

--- client/client.lua
+++ client/client.lua
@@ -142,16 +142,55 @@ RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
     InitAppearance()
 end)
 
-local function getConfigForPermission(hasPedPerms)
-    local config = {
-        ped = true,
-        headBlend = true,
-        faceFeatures = true,
-        headOverlays = true,
-        components = true,
-        props = true,
-        tattoos = true
+local function getComponentConfig()
+    return {
+        masks = not Config.DisableComponents.Masks,
+        upperBody = not Config.DisableComponents.UpperBody,
+        lowerBody = not Config.DisableComponents.LowerBody,
+        bags = not Config.DisableComponents.Bags,
+        shoes = not Config.DisableComponents.Shoes,
+        scarfAndChains = not Config.DisableComponents.ScarfAndChains,
+        bodyArmor = not Config.DisableComponents.BodyArmor,
+        shirts = not Config.DisableComponents.Shirts,
+        decals = not Config.DisableComponents.Decals,
+        jackets = not Config.DisableComponents.Jackets
     }
+end
+
+local function getPropConfig()
+    return {
+        hats = not Config.DisableProps.Hats,
+        glasses = not Config.DisableProps.Glasses,
+        ear = not Config.DisableProps.Ear,
+        watches = not Config.DisableProps.Watches,
+        bracelets = not Config.DisableProps.Bracelets
+    }
+end
+
+local function getDefaultConfig()
+    return {
+        ped = false,
+        headBlend = false,
+        faceFeatures = false,
+        headOverlays = false,
+        components = false,
+        componentConfig = getComponentConfig(),
+        props = false,
+        propConfig = getPropConfig(),
+        tattoos = false,
+        enableExit = true,
+    }
+end
+
+local function getConfigForPermission(hasPedPerms)
+    local config = getDefaultConfig()
+    config.ped = true
+    config.headBlend = true
+    config.faceFeatures = true
+    config.headOverlays = true
+    config.components = true
+    config.props = true
+    config.tattoos = true
 
     if Config.EnablePedMenu then
         config.ped = hasPedPerms
@@ -173,7 +212,7 @@ RegisterNetEvent('qb-clothes:client:CreateFirstCharacter', function()
         ClearPedDecorations(ped)
         QBCore.Functions.TriggerCallback("QBCore:HasPermission", function(permission)
             local config = getConfigForPermission(permission)
-            config.disableCancel = true
+            config.enableExit = false
             exports['fivem-appearance']:startPlayerCustomization(function(appearance)
                 if (appearance) then
                     TriggerServerEvent('fivem-appearance:server:saveAppearance', appearance)
@@ -205,63 +244,37 @@ function OpenShop(config, isPedMenu, shopType)
 end
 
 local function OpenClothingShop(isPedMenu)
-    local config = {
-        ped = false,
-        headBlend = false,
-        faceFeatures = false,
-        headOverlays = false,
-        components = true,
-        props = true,
-        tattoos = false
-    }
+    local config = getDefaultConfig()
+    config.components = true
+    config.props = true
+
     if isPedMenu then
-        config = {
-            ped = true,
-            headBlend = true,
-            faceFeatures = true,
-            headOverlays = true,
-            components = true,
-            props = true,
-            tattoos = true
-        }
+        config.ped = true
+        config.headBlend = true
+        config.faceFeatures = true
+        config.headOverlays = true
+        config.tattoos = true
     end
     OpenShop(config, isPedMenu, 'clothing')
 end
 
 local function OpenBarberShop()
-    OpenShop({
-        ped = false,
-        headBlend = false,
-        faceFeatures = false,
-        headOverlays = true,
-        components = false,
-        props = false,
-        tattoos = false
-    }, false, 'barber')
+    local config = getDefaultConfig()
+    config.headOverlays = true
+    OpenShop(config, false, 'barber')
 end
 
 local function OpenTattooShop()
-    OpenShop({
-        ped = false,
-        headBlend = false,
-        faceFeatures = false,
-        headOverlays = false,
-        components = false,
-        props = false,
-        tattoos = true
-    }, false, 'tattoo')
+    local config = getDefaultConfig()
+    config.tattoos = true
+    OpenShop(config, false, 'tattoo')
 end
 
 local function OpenSurgeonShop()
-    OpenShop({
-        ped = false,
-        headBlend = true,
-        faceFeatures = true,
-        headOverlays = false,
-        components = false,
-        props = false,
-        tattoos = false
-    }, false, 'surgeon')
+    local config = getDefaultConfig()
+    config.headBlend = true
+    config.faceFeatures = true
+    OpenShop(config, false, 'surgeon')
 end
 
 RegisterNetEvent('fivem-appearance:client:openClothingShop', OpenClothingShop)
