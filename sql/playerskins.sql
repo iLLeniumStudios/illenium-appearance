ffeace4f6c6e7fb1ddbb86e430ac8e8e829a76dc
TheiLLeniumStudios <104288623+TheiLLeniumStudios@users.noreply.github.com>
2022-12-31 20:23:26 +0000

    fix(sql): Add missing playerskins table file

---
+++ sql/playerskins.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS `playerskins` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `citizenid` varchar(255) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `skin` text NOT NULL,
+  `active` tinyint(2) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`),
+  KEY `citizenid` (`citizenid`),
+  KEY `active` (`active`)
+) ENGINE=InnoDB AUTO_INCREMENT DEFAULT CHARSET=utf8mb4;
