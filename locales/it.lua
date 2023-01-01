328f8c61def79970f2735d10c5f5674d7e1a2cb6
MrV <86290612+MrV-Development@users.noreply.github.com>
2023-01-01 23:22:55 +0100

    feat(lang): added italian translation (#193)
    * added italian translation
    
    * Update it.lua
    
    Co-authored-by: iLLeniumStudios <104288623+TheiLLeniumStudios@users.noreply.github.com>

---
+++ locales/it.lua
@@ -0,0 +1,150 @@
+Locales["it"] = {
+    modal = {
+        save = {
+            title = "Salva",
+            description = "Rimarrai sempre brutto"
+        },
+        exit = {
+            title = "Esci",
+            description = "Nessun cambiamento verrà salvato"
+        },
+        accept = "Si",
+        decline = "No"
+    },
+    ped = {
+        title = "Ped",
+        model = "Modello"
+    },
+    headBlend = {
+        title = "Eredità",
+        shape = {
+            title = "Faccia",
+            firstOption = "Padre",
+            secondOption = "Madre",
+            mix = "Mix"
+        },
+        skin = {
+            title = "Pelle",
+            firstOption = "Padre",
+            secondOption = "Madre",
+            mix = "Mix"
+        },
+        race = {
+            title = "Mix",
+            shape = "Forma",
+            skin = "Pelle",
+            mix = "Mix"
+        }
+    },
+    faceFeatures = {
+        title = "Caratteristiche del viso",
+        nose = {
+            title = "Naso",
+            width = "Larghezza",
+            height = "Altezza",
+            size = "Dimensione",
+            boneHeight = "Altezza dell'osso",
+            boneTwist = "Torsione ossea",
+            peakHeight = "Altezza della punta"
+        },
+        eyebrows = {
+            title = "Sopracciglia",
+            height = "Altezza",
+            depth = "Profondità"
+        },
+        cheeks = {
+            title = "Guance",
+            boneHeight = "Altezza dell'osso",
+            boneWidth = "Larghezza dell'osso",
+            width = "Larghezza"
+        },
+        eyesAndMouth = {
+            title = "Occhi e Bocca",
+            eyesOpening = "Apertura degli occhi",
+            lipsThickness = "Spessore delle labbra"
+        },
+        jaw = {
+            title = "Mascella",
+            width = "Larghezza",
+            size = "Dimensione"
+        },
+        chin = {
+            title = "Mento",
+            lowering = "Altezza",
+            length = "Lunghezza",
+            size = "Dimensione",
+            hole = "Mento a culo"
+        },
+        neck = {
+            title = "Collo",
+            thickness = "Spessore"
+        }
+    },
+    headOverlays = {
+        title = "Aspetto",
+        hair = {
+            title = "Capelli",
+            style = "Stile",
+            color = "Colore",
+            highlight = "Highlight",
+            texture = "Variante",
+            fade = "Sfumatura"
+        },
+        opacity = "Opacità",
+        style = "Stile",
+        color = "Colore",
+        secondColor = "Colore Secondario",
+        blemishes = "Macchie",
+        beard = "Barba",
+        eyebrows = "Sopracciglia",
+        ageing = "Invecchiamento",
+        makeUp = "Trucco",
+        blush = "Arrossamento",
+        complexion = "Carnagione",
+        sunDamage = "Danno dal Sole",
+        lipstick = "Rossetto",
+        moleAndFreckles = "Lentigini",
+        chestHair = "Peli del Petto",
+        bodyBlemishes = "Inestetismi del Corpo",
+        eyeColor = "Colore degli Occhi"
+    },
+    components = {
+        title = "Vestiti",
+        drawable = "Modello",
+        texture = "Variante",
+        mask = "Maschera",
+        upperBody = "Braccia",
+        lowerBody = "Pantaloni",
+        bags = "Borse",
+        shoes = "Scarpe",
+        scarfAndChains = "Sciarpa e Collane",
+        shirt = "Maglia",
+        bodyArmor = "Giubbotto",
+        decals = "Decalcomanie",
+        jackets = "Giacca"
+    },
+    props = {
+        title = "Accessori",
+        drawable = "Modello",
+        texture = "Variante",
+        hats = "Cappelli e Caschi",
+        glasses = "Occhiali",
+        ear = "Orecchie",
+        watches = "Orologi",
+        bracelets = "Bracciali"
+    },
+    tattoos = {
+        title = "Tatuaggi",
+        items = {
+            ZONE_TORSO = "Petto",
+            ZONE_HEAD = "Testa",
+            ZONE_LEFT_ARM = "Braccio Sinistro",
+            ZONE_RIGHT_ARM = "Braccio Destro",
+            ZONE_LEFT_LEG = "Gamba Sinistra",
+            ZONE_RIGHT_LEG = "Gamba Destra"
+        },
+        apply = "Applica",
+        delete = "Rimuovi",
+        deleteAll = "Rimuovi tutti i Tatuaggi"
+    }
+}
