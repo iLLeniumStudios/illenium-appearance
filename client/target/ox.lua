c260af3210bc4bc0a93e79258173a23a1c12df47
TheiLLeniumStudios <104288623+TheiLLeniumStudios@users.noreply.github.com>
2023-01-12 10:40:18 +0000

    feat(target): ox_target support

---
+++ client/target/ox.lua
@@ -0,0 +1,45 @@
+if not Target.IsOX() then return end
+
+local ZoneIDMap = {}
+
+local function convert(options)
+    local distance = options.distance
+    options = options.options
+    for _, v in pairs(options) do
+        v.onSelect = v.action
+        v.distance = v.distance or distance
+        v.name = v.name or v.label
+        v.groups = v.job or v.gang
+        v.type = nil
+        v.action = nil
+
+        v.job = nil
+        v.gang = nil
+        v.qtarget = true
+    end
+
+    return options
+end
+
+function Target.RemoveZone(zone)
+    exports["ox_target"]:removeZone(ZoneIDMap[zone])
+end
+
+function Target.AddTargetEntity(entity, parameters)
+    exports["ox_target"]:addLocalEntity(entity, convert(parameters))
+end
+
+function Target.AddBoxZone(name, coords, size, parameters)
+    local rotation = parameters.rotation
+    ZoneIDMap[name] = exports["ox_target"]:addBoxZone({
+        coords = coords,
+        size = size,
+        rotation = rotation,
+        debug = Config.Debug,
+        options = convert(parameters)
+    })
+end
+
+function Target.IsTargetStarted()
+    return GetResourceState("ox_target") == "started"
+end
