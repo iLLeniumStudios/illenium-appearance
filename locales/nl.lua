b92a30c6e65502dc3de34d5f1840274c27cc67cf
RazerGhost <90572937+RazerGhost@users.noreply.github.com>
2023-01-12 15:32:52 +0100

    feat(translations): Dutch translation mostly (#226)
    * Dutch translation mostly
    
    * removed as requested
    
    Co-authored-by: iLLeniumStudios <104288623+TheiLLeniumStudios@users.noreply.github.com>

---
+++ locales/nl.lua
@@ -0,0 +1,152 @@
+Locales["nl"] = {
+    modal = {
+        save = {
+            title = "Veranderingen Toepassen",
+            description = "Je blijft lelijk"
+        },
+        exit = {
+            title = "Menu Verlaten",
+            description = "Veranderingen worden niet opgeslagen"
+        },
+        accept = "Ja",
+        decline = "Nee"
+    },
+    ped = {
+        title = "Persoon",
+        model = "Model"
+    },
+    headBlend = {
+        title = "Erfenis",
+        shape = {
+            title = "Gezicht",
+            firstOption = "Vader",
+            secondOption = "Moeder",
+            mix = "Mix"
+        },
+        skin = {
+            title = "Huid",
+            firstOption = "Vader",
+            secondOption = "Moeder",
+            mix = "Mix"
+        },
+        race = {
+            title = "Ras",
+            shape = "Vorm",
+            skin = "Huid",
+            mix = "Mix"
+        }
+    },
+    faceFeatures = {
+        title = "Gezichtskenmerken",
+        nose = {
+            title = "Neus",
+            width = "Breedte",
+            height = "Hoogte",
+            size = "Groote",
+            boneHeight = "Bot hoogte",
+            boneTwist = "Bot twist",
+            peakHeight = "Neus hoogte"
+        },
+        eyebrows = {
+            title = "Wenkbrouwen",
+            height = "Hoogte",
+            depth = "Diepte"
+        },
+        cheeks = {
+            title = "Wangen",
+            boneHeight = "Bot hoogte",
+            boneWidth = "Bone width",
+            width = "Breedte"
+        },
+        eyesAndMouth = {
+            title = "Ogen en Mond",
+            eyesOpening = "Oog opening",
+            lipsThickness = "Lip dikte"
+        },
+        jaw = {
+            title = "Kaak",
+            width = "Breedte",
+            size = "Groote"
+        },
+        chin = {
+            title = "Kin",
+            lowering = "Verlaging",
+            length = "Lengte",
+            size = "Groote",
+            hole = "Gat groote"
+        },
+        neck = {
+            title = "Nek",
+            thickness = "Dikte"
+        }
+    },
+    headOverlays = {
+        title = "Uiterlijk",
+        hair = {
+            title = "Haar",
+            style = "Stijl",
+            color = "Kleur",
+            highlight = "Highlight",
+            texture = "Textuur",
+            fade = "Fade"
+        },
+        opacity = "Helderheid",
+        style = "Stijl",
+        color = "Kleur",
+        secondColor = "Secundaire Kleur",
+        blemishes = "Vlekken",
+        beard = "Baard",
+        eyebrows = "Wenkbrouwen",
+        ageing = "Veroudering",
+        makeUp = "Make up",
+        blush = "Blush",
+        complexion = "Tint",
+        sunDamage = "Zonnebrand",
+        lipstick = "Lippenstift",
+        moleAndFreckles = "Wrat en Sproeten",
+        chestHair = "Borst haar",
+        bodyBlemishes = "Lichaamelijke vlekken",
+        eyeColor = "Oog kleur"
+    },
+    components = {
+        title = "Kleding",
+        drawable = "Tekenbaar",
+        texture = "Textuur",
+        mask = "Masker",
+        upperBody = "Handen",
+        lowerBody = "Benen",
+        bags = "Tassen en parachute",
+        shoes = "Schoenen",
+        scarfAndChains = "Sjaal en kettingen",
+        shirt = "Shirt",
+        bodyArmor = "Vesten",
+        decals = "Stickers",
+        jackets = "Jassen",
+        head = "Hoofd"
+    },
+    props = {
+        title = "Sieraden",
+        drawable = "Tekenbaar",
+        texture = "Textuur",
+        hats = "Hoeden en helmen",
+        glasses = "Brillen",
+        ear = "Oor",
+        watches = "Horloges",
+        bracelets = "Armbanden"
+    },
+    tattoos = {
+        title = "Tattoos",
+        items = {
+            ZONE_TORSO = "Borstkas",
+            ZONE_HEAD = "Hoofd",
+            ZONE_LEFT_ARM = "Linker arm",
+            ZONE_RIGHT_ARM = "Rechter arm",
+            ZONE_LEFT_LEG = "Linker been",
+            ZONE_RIGHT_LEG = "Rechter been"
+        },
+        apply = "Toepassen",
+        delete = "Verwijder",
+        deleteAll = "Verwijder alle Tattoos",
+        opacity = "Helderheid"
+    }
+}
