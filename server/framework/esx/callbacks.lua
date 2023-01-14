acafde0eb21e29beda9c813832a1c9f6284b7086
Katoteki <58405477+katotekii@users.noreply.github.com>
2023-01-14 21:44:04 +0100

    Adding more compatibility to callback
    Adding more compatibility to callback "esx_skin:getPlayerSkin". Since alot of people still using "sex" to determine whether ped is male or female

--- server/framework/esx/callbacks.lua
+++ server/framework/esx/callbacks.lua
@@ -3,16 +3,24 @@ if not Framework.ESX() then return end
 local ESX = exports["es_extended"]:getSharedObject()
 
 ESX.RegisterServerCallback("esx_skin:getPlayerSkin", function(source, cb)
-	local Player = ESX.GetPlayerFromId(source)
-
+    local Player = ESX.GetPlayerFromId(source)
     local appearance = Framework.GetAppearance(Player.identifier)
+
+    if appearance.model then
+        if appearance.model == 'mp_m_freemode_01' then
+            appearance.sex = 0
+        elseif appearance.model == 'mp_f_freemode_01' then
+            appearance.sex = 1
+        else
+            appearance.sex = 0
+        end
+    end
 
     cb(appearance, {
         skin_male = Player.job.skin_male,
         skin_female = Player.job.skin_female
     })
 end)
-
 lib.callback.register("illenium-appearance:server:esx:getGradesForJob", function(jobName)
     return Database.JobGrades.GetByJobName(jobName)
 end)
