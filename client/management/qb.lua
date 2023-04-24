db5090dd77f711e7872fb268a258c65c805c7594
iLLeniumStudios <104288623+TheiLLeniumStudios@users.noreply.github.com>
2023-04-24 10:13:07 +0200

    feat(management/qbox): qbx-management support for BossManagedOutfits (#296)

---
+++ client/management/qb.lua
@@ -0,0 +1,18 @@
+if not Management.IsQB() then return end
+
+function Management.AddItems()
+    local menuItem = {
+        header = _L("outfitManagement.title"),
+        icon = "fa-solid fa-shirt",
+        params = {
+            event = "illenium-appearance:client:OutfitManagementMenu"
+        }
+    }
+    menuItem.txt = _L("outfitManagement.jobText")
+    menuItem.params.args.type = "Job"
+    Management.ItemIDs.Boss = exports[Management.ResourceName]:AddBossMenuItem(menuItem)
+
+    menuItem.txt = _L("outfitManagement.gangText")
+    menuItem.params.args.type = "Gang"
+    Management.ItemIDs.Gang = exports[Management.ResourceName]:AddGangMenuItem(menuItem)
+end
