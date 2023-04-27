a82994b2e6d7bc8dd03e3dd4ecf394f99ee1f612
iLLeniumStudios <104288623+TheiLLeniumStudios@users.noreply.github.com>
2023-04-27 12:21:52 +0200

    refactor(client): Move logic to separate lua files (#300)

--- client/client.lua
+++ client/client.lua
@@ -1,66 +1,5 @@
 local client = client
-
-local currentZone = nil
-
 local reloadSkinTimer = GetGameTimer()
-
-local TargetPeds = {
-    Store = {},
-    ClothingRoom = {},
-    PlayerOutfitRoom = {}
-}
-
-local Zones = {
-    Store = {},
-    ClothingRoom = {},
-    PlayerOutfitRoom = {}
-}
-
-local function RemoveTargetPeds(peds)
-    for i = 1, #peds, 1 do
-        DeletePed(peds[i])
-    end
-end
-
-local function RemoveTargets()
-    if Config.EnablePedsForShops then
-        RemoveTargetPeds(TargetPeds.Store)
-    else
-        for k, v in pairs(Config.Stores) do
-            Target.RemoveZone(v.type .. k)
-        end
-    end
-
-    if Config.EnablePedsForClothingRooms then
-        RemoveTargetPeds(TargetPeds.ClothingRoom)
-    else
-        for k, v in pairs(Config.ClothingRooms) do
-            Target.RemoveZone("clothing_" .. (v.job or v.gang) .. k)
-        end
-    end
-
-    if Config.EnablePedsForPlayerOutfitRooms then
-        RemoveTargetPeds(TargetPeds.PlayerOutfitRoom)
-    else
-        for k in pairs(Config.PlayerOutfitRooms) do
-            Target.RemoveZone("playeroutfitroom_" .. k)
-        end
-    end
-end
-
-local function RemoveZones()
-    for i = 1, #Zones.Store do
-        if Zones.Store[i]["remove"] then
-            Zones.Store[i]:remove()
-        end
-    end
-    for i = 1, #Zones.ClothingRoom do
-        Zones.ClothingRoom[i]:remove()
-    end
-    for i = 1, #Zones.PlayerOutfitRoom do
-        Zones.PlayerOutfitRoom[i]:remove()
-    end
-end
 
 local function LoadPlayerUniform()
     lib.callback("illenium-appearance:server:getUniform", false, function(uniformData)
@@ -137,28 +76,6 @@ AddEventHandler("onResourceStart", function(resource)
         InitAppearance()
     end
 end)
-
-AddEventHandler("onResourceStop", function(resource)
-    if resource == GetCurrentResourceName() then
-        if Config.UseTarget and Target.IsTargetStarted() then
-            RemoveTargets()
-        else
-            RemoveZones()
-        end
-        if Config.UseRadialMenu then
-            if Config.UseOxRadial and GetResourceState('ox_lib') == "started" then
-                Radial.RemoveOption()
-            elseif GetResourceState("qb-radialmenu") == "started" then
-                Radial.RemoveOption()
-            end
-        end
-        if Config.BossManagedOutfits then
-            Management.RemoveItems()
-        end
-    end
-end)
-
-
 
 local function getNewCharacterConfig()
     local config = GetDefaultConfig()
@@ -248,25 +165,6 @@ local function OpenClothingShop(isPedMenu)
         config.tattoos = not Config.RCoreTattoosCompatibility and true
     end
     OpenShop(config, isPedMenu, "clothing")
-end
-
-local function OpenBarberShop()
-    local config = GetDefaultConfig()
-    config.headOverlays = true
-    OpenShop(config, false, "barber")
-end
-
-local function OpenTattooShop()
-    local config = GetDefaultConfig()
-    config.tattoos = true
-    OpenShop(config, false, "tattoo")
-end
-
-local function OpenSurgeonShop()
-    local config = GetDefaultConfig()
-    config.headBlend = true
-    config.faceFeatures = true
-    OpenShop(config, false, "surgeon")
 end
 
 RegisterNetEvent("illenium-appearance:client:openClothingShop", OpenClothingShop)
@@ -711,7 +609,8 @@ function OpenMenu(isPedMenu, menuType, menuData)
         menuItems[#menuItems + 1] = {
             title = _L("clothing.outfits.civilian.title"),
             description = _L("clothing.outfits.civilian.description"),
-            event = "illenium-appearance:client:reloadSkin"
+            event = "illenium-appearance:client:reloadSkin",
+            args = true
         }
 
         local workOutfitsMenuID = "illenium_appearance_work_outfits_menu"
@@ -736,17 +635,11 @@ RegisterNetEvent("illenium-appearance:client:openClothingShopMenu", function(isP
     OpenMenu(isPedMenu, "default")
 end)
 
-RegisterNetEvent("illenium-appearance:client:OpenBarberShop", function()
-    OpenBarberShop()
-end)
+RegisterNetEvent("illenium-appearance:client:OpenBarberShop", OpenBarberShop)
 
-RegisterNetEvent("illenium-appearance:client:OpenTattooShop", function()
-    OpenTattooShop()
-end)
+RegisterNetEvent("illenium-appearance:client:OpenTattooShop", OpenTattooShop)
 
-RegisterNetEvent("illenium-appearance:client:OpenSurgeonShop", function()
-    OpenSurgeonShop()
-end)
+RegisterNetEvent("illenium-appearance:client:OpenSurgeonShop", OpenSurgeonShop)
 
 RegisterNetEvent("illenium-appearance:client:changeOutfit", function(data)
     local pedModel = client.getPedModel(cache.ped)
@@ -818,8 +711,8 @@ local function InCooldown()
     return (GetGameTimer() - reloadSkinTimer) < Config.ReloadSkinCooldown
 end
 
-RegisterNetEvent("illenium-appearance:client:reloadSkin", function()
-    if InCooldown() or Framework.CheckPlayerMeta() or cache.vehicle or IsPedFalling(cache.ped) then
+RegisterNetEvent("illenium-appearance:client:reloadSkin", function(bypassChecks)
+    if not bypassChecks and InCooldown() or Framework.CheckPlayerMeta() or cache.vehicle or IsPedFalling(cache.ped) then
         lib.notify({
             title = _L("commands.reloadskin.failure.title"),
             description = _L("commands.reloadskin.failure.description"),
@@ -863,369 +756,5 @@ RegisterNetEvent("illenium-appearance:client:ClearStuckProps", function()
         DeleteObject(v)
         DeleteEntity(v)
       end
-    end
-end)
-
-local function isPlayerAllowedForOutfitRoom(outfitRoom)
-    local isAllowed = false
-    local count = #outfitRoom.citizenIDs
-    for i = 1, count, 1 do
-        if Framework.IsPlayerAllowed(outfitRoom.citizenIDs[i]) then
-            isAllowed = true
-            break
-        end
-    end
-    return isAllowed or not outfitRoom.citizenIDs or count == 0
-end
-
-local function OpenOutfitRoom(outfitRoom)
-    local isAllowed = isPlayerAllowedForOutfitRoom(outfitRoom)
-    if isAllowed then
-        OpenMenu(nil, "outfit")
-    end
-end
-
-local function getPlayerJobOutfits(clothingRoom)
-    local outfits = {}
-    local gender = Framework.GetGender()
-    local gradeLevel = clothingRoom.job and Framework.GetJobGrade() or Framework.GetGangGrade()
-    local jobName = clothingRoom.job and client.job.name or client.gang.name
-
-    if Config.BossManagedOutfits then
-        local mType = clothingRoom.job and "Job" or "Gang"
-        local result = lib.callback.await("illenium-appearance:server:getManagementOutfits", false, mType, gender)
-        for i = 1, #result, 1 do
-            outfits[#outfits + 1] = {
-                type = mType,
-                model = result[i].model,
-                components = result[i].components,
-                props = result[i].props,
-                disableSave = true,
-                name = result[i].name
-            }
-        end
-    else
-        for i = 1, #Config.Outfits[jobName][gender], 1 do
-            for _, v in pairs(Config.Outfits[jobName][gender][i].grades) do
-                if v == gradeLevel then
-                    outfits[#outfits + 1] = Config.Outfits[jobName][gender][i]
-                    outfits[#outfits].gender = gender
-                    outfits[#outfits].jobName = jobName
-                end
-            end
-        end
-    end
-
-    return outfits
-end
-
-RegisterNetEvent("illenium-appearance:client:OpenClothingRoom", function()
-    local clothingRoom = Config.ClothingRooms[currentZone.index]
-    local outfits = getPlayerJobOutfits(clothingRoom)
-    TriggerEvent("illenium-appearance:client:openJobOutfitsMenu", outfits)
-end)
-
-RegisterNetEvent("illenium-appearance:client:OpenPlayerOutfitRoom", function()
-    local outfitRoom = Config.PlayerOutfitRooms[currentZone.index]
-    OpenOutfitRoom(outfitRoom)
-end)
-
-local function CheckDuty()
-    return not Config.OnDutyOnlyClothingRooms or (Config.OnDutyOnlyClothingRooms and client.job.onduty)
-end
-
-local function lookupZoneIndexFromID(zones, id)
-    for i = 1, #zones do
-        if zones[i].id == id then
-            return i
-        end
-    end
-end
-
-local function onStoreEnter(data)
-    local index = lookupZoneIndexFromID(Zones.Store, data.id)
-    local store = Config.Stores[index]
-
-    local jobName = (store.job and client.job.name) or (store.gang and client.gang.name)
-    if jobName == (store.job or store.gang) then
-        currentZone = {
-            name = store.type,
-            index = index
-        }
-        local prefix = Config.UseRadialMenu and "" or "[E] "
-        if currentZone.name == "clothing" then
-            lib.showTextUI(prefix .. string.format(_L("textUI.clothing"), Config.ClothingCost), Config.TextUIOptions)
-        elseif currentZone.name == "barber" then
-            lib.showTextUI(prefix .. string.format(_L("textUI.barber"), Config.BarberCost), Config.TextUIOptions)
-        elseif currentZone.name == "tattoo" then
-            lib.showTextUI(prefix .. string.format(_L("textUI.tattoo"), Config.TattooCost), Config.TextUIOptions)
-        elseif currentZone.name == "surgeon" then
-            lib.showTextUI(prefix .. string.format(_L("textUI.surgeon"), Config.SurgeonCost), Config.TextUIOptions)
-        end
-        Radial.AddOption(currentZone)
-    end
-end
-
-local function onClothingRoomEnter(data)
-    local index = lookupZoneIndexFromID(Zones.ClothingRoom, data.id)
-    local clothingRoom = Config.ClothingRooms[index]
-
-    local jobName = clothingRoom.job and client.job.name or client.gang.name
-    if jobName == (clothingRoom.job or clothingRoom.gang) then
-        if CheckDuty() or clothingRoom.gang then
-            currentZone = {
-                name = "clothingRoom",
-                index = index
-            }
-            local prefix = Config.UseRadialMenu and "" or "[E] "
-            lib.showTextUI(prefix .. _L("textUI.clothingRoom"), Config.TextUIOptions)
-            Radial.AddOption(currentZone)
-        end
-    end
-end
-
-local function onPlayerOutfitRoomEnter(data)
-    local index = lookupZoneIndexFromID(Zones.PlayerOutfitRoom, data.id)
-    local playerOutfitRoom = Config.PlayerOutfitRooms[index]
-
-    local isAllowed = isPlayerAllowedForOutfitRoom(playerOutfitRoom)
-    if isAllowed then
-        currentZone = {
-            name = "playerOutfitRoom",
-            index = index
-        }
-        local prefix = Config.UseRadialMenu and "" or "[E] "
-        lib.showTextUI(prefix .. _L("textUI.playerOutfitRoom"), Config.TextUIOptions)
-        Radial.AddOption(currentZone)
-    end
-end
-
-local function onZoneExit()
-    currentZone = nil
-    Radial.RemoveOption()
-    lib.hideTextUI()
-end
-
-local function SetupZone(store, onEnter, onExit)
-    if Config.RCoreTattoosCompatibility and store.type == "tattoo" then
-        return {}
-    end
-
-    if Config.UseRadialMenu or store.usePoly then
-        return lib.zones.poly({
-            points = store.points,
-            debug = Config.Debug,
-            onEnter = onEnter,
-            onExit = onExit
-        })
-    end
-
-    return lib.zones.box({
-        coords = store.coords,
-        size = store.size,
-        rotation = store.rotation,
-        debug = Config.Debug,
-        onEnter = onEnter,
-        onExit = onExit
-    })
-end
-
-local function SetupStoreZones()
-    for _, v in pairs(Config.Stores) do
-        Zones.Store[#Zones.Store + 1] = SetupZone(v, onStoreEnter, onZoneExit)
-    end
-end
-
-local function SetupClothingRoomZones()
-    for _, v in pairs(Config.ClothingRooms) do
-        Zones.ClothingRoom[#Zones.ClothingRoom + 1] = SetupZone(v, onClothingRoomEnter, onZoneExit)
-    end
-end
-
-local function SetupPlayerOutfitRoomZones()
-    for _, v in pairs(Config.PlayerOutfitRooms) do
-        Zones.PlayerOutfitRoom[#Zones.PlayerOutfitRoom + 1] = SetupZone(v, onPlayerOutfitRoomEnter, onZoneExit)
-    end
-end
-
-local function SetupZones()
-    SetupStoreZones()
-    SetupClothingRoomZones()
-    SetupPlayerOutfitRoomZones()
-end
-
-local function EnsurePedModel(pedModel)
-    RequestModel(pedModel)
-    while not HasModelLoaded(pedModel) do
-        Wait(10)
-    end
-end
-
-local function CreatePedAtCoords(pedModel, coords, scenario)
-    pedModel = type(pedModel) == "string" and joaat(pedModel) or pedModel
-    EnsurePedModel(pedModel)
-    local ped = CreatePed(0, pedModel, coords.x, coords.y, coords.z - 0.98, coords.w, false, false)
-    TaskStartScenarioInPlace(ped, scenario, true)
-    FreezeEntityPosition(ped, true)
-    SetEntityVisible(ped, true)
-    SetEntityInvincible(ped, true)
-    PlaceObjectOnGroundProperly(ped)
-    SetBlockingOfNonTemporaryEvents(ped, true)
-    return ped
-end
-
-local function SetupStoreTarget(targetConfig, action, k, v)
-    local parameters = {
-        options = {{
-            type = "client",
-            action = action,
-            icon = targetConfig.icon,
-            label = targetConfig.label
-        }},
-        distance = targetConfig.distance,
-        rotation = v.rotation
-    }
-
-    if Config.EnablePedsForShops then
-        TargetPeds.Store[k] = CreatePedAtCoords(v.targetModel or targetConfig.model, v.coords, v.targetScenario or targetConfig.scenario)
-        Target.AddTargetEntity(TargetPeds.Store[k], parameters)
-    elseif v.usePoly then
-        Target.AddPolyZone(v.type .. k, v.points, parameters)
-    else
-        Target.AddBoxZone(v.type .. k, v.coords, v.size, parameters)
-    end
-end
-
-local function SetupStoreTargets()
-    for k, v in pairs(Config.Stores) do
-        local targetConfig = Config.TargetConfig[v.type]
-        local action
-
-        if v.type == "barber" then
-            action = OpenBarberShop
-        elseif v.type == "clothing" then
-            action = function()
-                TriggerEvent("illenium-appearance:client:openClothingShopMenu")
-            end
-        elseif v.type == "tattoo" then
-            action = OpenTattooShop
-        elseif v.type == "surgeon" then
-            action = OpenSurgeonShop
-        end
-
-        if not (Config.RCoreTattoosCompatibility and v.type == "tattoo") then
-            SetupStoreTarget(targetConfig, action, k, v)
-        end
-    end
-end
-
-local function SetupClothingRoomTargets()
-    for k, v in pairs(Config.ClothingRooms) do
-        local targetConfig = Config.TargetConfig["clothingroom"]
-        local action = function()
-            local outfits = getPlayerJobOutfits(v)
-            TriggerEvent("illenium-appearance:client:openJobOutfitsMenu", outfits)
-        end
-
-        local parameters = {
-            options = {{
-                type = "client",
-                action = action,
-                icon = targetConfig.icon,
-                label = targetConfig.label,
-                canInteract = v.job and CheckDuty or nil,
-                job = v.job,
-                gang = v.gang
-            }},
-            distance = targetConfig.distance,
-            rotation = v.rotation
-        }
-
-        local key = "clothing_" .. (v.job or v.gang) .. k
-        if Config.EnablePedsForClothingRooms then
-            TargetPeds.ClothingRoom[k] = CreatePedAtCoords(v.targetModel or targetConfig.model, v.coords, v.targetScenario or targetConfig.scenario)
-            Target.AddTargetEntity(TargetPeds.ClothingRoom[k], parameters)
-        elseif v.usePoly then
-            Target.AddPolyZone(key, v.points, parameters)
-        else
-            Target.AddBoxZone(key, v.coords, v.size, parameters)
-        end
-    end
-end
-
-local function SetupPlayerOutfitRoomTargets()
-    for k, v in pairs(Config.PlayerOutfitRooms) do
-        local targetConfig = Config.TargetConfig["playeroutfitroom"]
-
-        local parameters = {
-            options = {{
-                type = "client",
-                action = function()
-                    OpenOutfitRoom(v)
-                end,
-                icon = targetConfig.icon,
-                label = targetConfig.label,
-                canInteract = function()
-                    return isPlayerAllowedForOutfitRoom(v)
-                end
-            }},
-            distance = targetConfig.distance,
-            rotation = v.rotation
-        }
-
-        if Config.EnablePedsForPlayerOutfitRooms then
-            TargetPeds.PlayerOutfitRoom[k] = CreatePedAtCoords(v.targetModel or targetConfig.model, v.coords, v.targetScenario or targetConfig.scenario)
-            Target.AddTargetEntity(TargetPeds.PlayerOutfitRoom[k], parameters)
-        elseif v.usePoly then
-            Target.AddPolyZone("playeroutfitroom_" .. k, v.points, parameters)
-        else
-            Target.AddBoxZone("playeroutfitroom_" .. k, v.coords, v.size, parameters)
-        end
-    end
-end
-
-local function SetupTargets()
-    SetupStoreTargets()
-    SetupClothingRoomTargets()
-    SetupPlayerOutfitRoomTargets()
-end
-
-local function ZonesLoop()
-    Wait(1000)
-    while true do
-        local sleep = 1000
-        if currentZone then
-            sleep = 5
-            if IsControlJustReleased(0, 38) then
-                if currentZone.name == "clothingRoom" then
-                    local clothingRoom = Config.ClothingRooms[currentZone.index]
-                    local outfits = getPlayerJobOutfits(clothingRoom)
-                    TriggerEvent("illenium-appearance:client:openJobOutfitsMenu", outfits)
-                elseif currentZone.name == "playerOutfitRoom" then
-                    local outfitRoom = Config.PlayerOutfitRooms[currentZone.index]
-                    OpenOutfitRoom(outfitRoom)
-                elseif currentZone.name == "clothing" then
-                    TriggerEvent("illenium-appearance:client:openClothingShopMenu")
-                elseif currentZone.name == "barber" then
-                    OpenBarberShop()
-                elseif currentZone.name == "tattoo" then
-                    OpenTattooShop()
-                elseif currentZone.name == "surgeon" then
-                    OpenSurgeonShop()
-                end
-            end
-        end
-        Wait(sleep)
-    end
-end
-
-CreateThread(function()
-    if Config.UseTarget then
-        SetupTargets()
-    else
-        SetupZones()
-        if not Config.UseRadialMenu then
-            ZonesLoop()
-        end
     end
 end)
--- client/target/target.lua
+++ client/target/target.lua
@@ -1,3 +1,11 @@
+if not Config.UseTarget then return end
+
+local TargetPeds = {
+    Store = {},
+    ClothingRoom = {},
+    PlayerOutfitRoom = {}
+}
+
 Target = {}
 
 function Target.IsOX()
@@ -7,3 +15,178 @@ end
 function Target.IsQB()
     return GetResourceState("qb-target") ~= "missing"
 end
+
+local function RemoveTargetPeds(peds)
+    for i = 1, #peds, 1 do
+        DeletePed(peds[i])
+    end
+end
+
+local function RemoveTargets()
+    if Config.EnablePedsForShops then
+        RemoveTargetPeds(TargetPeds.Store)
+    else
+        for k, v in pairs(Config.Stores) do
+            Target.RemoveZone(v.type .. k)
+        end
+    end
+
+    if Config.EnablePedsForClothingRooms then
+        RemoveTargetPeds(TargetPeds.ClothingRoom)
+    else
+        for k, v in pairs(Config.ClothingRooms) do
+            Target.RemoveZone("clothing_" .. (v.job or v.gang) .. k)
+        end
+    end
+
+    if Config.EnablePedsForPlayerOutfitRooms then
+        RemoveTargetPeds(TargetPeds.PlayerOutfitRoom)
+    else
+        for k in pairs(Config.PlayerOutfitRooms) do
+            Target.RemoveZone("playeroutfitroom_" .. k)
+        end
+    end
+end
+
+AddEventHandler("onResourceStop", function(resource)
+    if resource == GetCurrentResourceName() then
+        if Target.IsTargetStarted() then
+            RemoveTargets()
+        end
+    end
+end)
+
+local function CreatePedAtCoords(pedModel, coords, scenario)
+    pedModel = type(pedModel) == "string" and joaat(pedModel) or pedModel
+    lib.requestModel(pedModel)
+    local ped = CreatePed(0, pedModel, coords.x, coords.y, coords.z - 0.98, coords.w, false, false)
+    TaskStartScenarioInPlace(ped, scenario, true)
+    FreezeEntityPosition(ped, true)
+    SetEntityVisible(ped, true)
+    SetEntityInvincible(ped, true)
+    PlaceObjectOnGroundProperly(ped)
+    SetBlockingOfNonTemporaryEvents(ped, true)
+    return ped
+end
+
+local function SetupStoreTarget(targetConfig, action, k, v)
+    local parameters = {
+        options = {{
+            type = "client",
+            action = action,
+            icon = targetConfig.icon,
+            label = targetConfig.label
+        }},
+        distance = targetConfig.distance,
+        rotation = v.rotation
+    }
+
+    if Config.EnablePedsForShops then
+        TargetPeds.Store[k] = CreatePedAtCoords(v.targetModel or targetConfig.model, v.coords, v.targetScenario or targetConfig.scenario)
+        Target.AddTargetEntity(TargetPeds.Store[k], parameters)
+    elseif v.usePoly then
+        Target.AddPolyZone(v.type .. k, v.points, parameters)
+    else
+        Target.AddBoxZone(v.type .. k, v.coords, v.size, parameters)
+    end
+end
+
+local function SetupStoreTargets()
+    for k, v in pairs(Config.Stores) do
+        local targetConfig = Config.TargetConfig[v.type]
+        local action
+
+        if v.type == "barber" then
+            action = OpenBarberShop
+        elseif v.type == "clothing" then
+            action = function()
+                TriggerEvent("illenium-appearance:client:openClothingShopMenu")
+            end
+        elseif v.type == "tattoo" then
+            action = OpenTattooShop
+        elseif v.type == "surgeon" then
+            action = OpenSurgeonShop
+        end
+
+        if not (Config.RCoreTattoosCompatibility and v.type == "tattoo") then
+            SetupStoreTarget(targetConfig, action, k, v)
+        end
+    end
+end
+
+local function SetupClothingRoomTargets()
+    for k, v in pairs(Config.ClothingRooms) do
+        local targetConfig = Config.TargetConfig["clothingroom"]
+        local action = function()
+            local outfits = GetPlayerJobOutfits(v)
+            TriggerEvent("illenium-appearance:client:openJobOutfitsMenu", outfits)
+        end
+
+        local parameters = {
+            options = {{
+                type = "client",
+                action = action,
+                icon = targetConfig.icon,
+                label = targetConfig.label,
+                canInteract = v.job and CheckDuty or nil,
+                job = v.job,
+                gang = v.gang
+            }},
+            distance = targetConfig.distance,
+            rotation = v.rotation
+        }
+
+        local key = "clothing_" .. (v.job or v.gang) .. k
+        if Config.EnablePedsForClothingRooms then
+            TargetPeds.ClothingRoom[k] = CreatePedAtCoords(v.targetModel or targetConfig.model, v.coords, v.targetScenario or targetConfig.scenario)
+            Target.AddTargetEntity(TargetPeds.ClothingRoom[k], parameters)
+        elseif v.usePoly then
+            Target.AddPolyZone(key, v.points, parameters)
+        else
+            Target.AddBoxZone(key, v.coords, v.size, parameters)
+        end
+    end
+end
+
+local function SetupPlayerOutfitRoomTargets()
+    for k, v in pairs(Config.PlayerOutfitRooms) do
+        local targetConfig = Config.TargetConfig["playeroutfitroom"]
+
+        local parameters = {
+            options = {{
+                type = "client",
+                action = function()
+                    OpenOutfitRoom(v)
+                end,
+                icon = targetConfig.icon,
+                label = targetConfig.label,
+                canInteract = function()
+                    return IsPlayerAllowedForOutfitRoom(v)
+                end
+            }},
+            distance = targetConfig.distance,
+            rotation = v.rotation
+        }
+
+        if Config.EnablePedsForPlayerOutfitRooms then
+            TargetPeds.PlayerOutfitRoom[k] = CreatePedAtCoords(v.targetModel or targetConfig.model, v.coords, v.targetScenario or targetConfig.scenario)
+            Target.AddTargetEntity(TargetPeds.PlayerOutfitRoom[k], parameters)
+        elseif v.usePoly then
+            Target.AddPolyZone("playeroutfitroom_" .. k, v.points, parameters)
+        else
+            Target.AddBoxZone("playeroutfitroom_" .. k, v.coords, v.size, parameters)
+        end
+    end
+end
+
+local function SetupTargets()
+    SetupStoreTargets()
+    SetupClothingRoomTargets()
+    SetupPlayerOutfitRoomTargets()
+end
+
+CreateThread(function()
+    if Config.UseTarget then
+        SetupTargets()
+    end
+end)
