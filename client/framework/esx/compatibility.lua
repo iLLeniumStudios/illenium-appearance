9f89d44c628dc3dd986f2624ec04a42f5578d843
iLLeniumStudios <104288623+TheiLLeniumStudios@users.noreply.github.com>
2023-05-06 15:21:45 +0200

    fix(esx): Only restore loadout if exists

--- client/framework/esx/compatibility.lua
+++ client/framework/esx/compatibility.lua
@@ -35,7 +35,7 @@ RegisterNetEvent("skinchanger:loadSkin", function(skin, cb)
     else -- add validation invisible when failed registration (maybe server restarted when apply skin)
         SetInitialClothes(Config.InitialPlayerClothes[Framework.GetGender(true)])
     end
-    if Framework.PlayerData then
+    if Framework.PlayerData and Framework.PlayerData.loadout then
         TriggerEvent("esx:restoreLoadout")
     end
     Framework.CachePed()
