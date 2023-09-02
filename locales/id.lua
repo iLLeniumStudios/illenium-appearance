6a1abbcd54169fe59bc9ec4b27cd193bcaebdcca
Reyghita Hafizh <87755064+reyyghi@users.noreply.github.com>
2023-09-02 12:57:20 +0700

    Create id.lua

---
+++ locales/id.lua
@@ -0,0 +1,367 @@
+Locales["id"] = {
+    UI = {
+        modal = {
+            save = {
+                title = "Simpan Perubahan",
+                description = "Kamu akan tetap jelek"
+            },
+            exit = {
+                title = "Keluar",
+                description = "Tidak ada perubahan yang akan di simpan"
+            },
+            accept = "Iya",
+            decline = "Tidak"
+        },
+        ped = {
+            title = "Karakter",
+            model = "Pilihan"
+        },
+        headBlend = {
+            title = "Bagian Kepala",
+            shape = {
+                title = "Muka",
+                firstOption = "Ayah",
+                secondOption = "Ibu",
+                mix = "Campuran"
+            },
+            skin = {
+                title = "Kulit",
+                firstOption = "Ayah",
+                secondOption = "Ibu",
+                mix = "Campuran"
+            },
+            race = {
+                title = "Ras",
+                shape = "Bentuk",
+                skin = "Kulit",
+                mix = "Campuran"
+            }
+        },
+        faceFeatures = {
+            title = "Fitur Wajah",
+            nose = {
+                title = "Hidung",
+                width = "Lebar",
+                height = "Tinggi",
+                size = "Besar",
+                boneHeight = "Tinggi Tulang",
+                boneTwist = "Lengkuk Tulang",
+                peakHeight = "Kemancungan"
+            },
+            eyebrows = {
+                title = "Alis",
+                height = "Tinggi",
+                depth = "Kedalaman"
+            },
+            cheeks = {
+                title = "Pipi",
+                boneHeight = "Tinggi Tulang",
+                boneWidth = "Lebar Tulang",
+                width = "Lebar"
+            },
+            eyesAndMouth = {
+                title = "Mata Dan Mulut",
+                eyesOpening = "Buka Mata",
+                lipsThickness = "Ketebalan Bibir"
+            },
+            jaw = {
+                title = "Rahang",
+                width = "Lebar Rahang",
+                size = "Besar"
+            },
+            chin = {
+                title = "Dagu",
+                lowering = "Penurunan",
+                length = "Panjang",
+                size = "Besar",
+                hole = "Ukuran Lubang"
+            },
+            neck = {
+                title = "Leher",
+                thickness = "Ketebalan"
+            }
+        },
+        headOverlays = {
+            title = "Penampilan",
+            hair = {
+                title = "Rambut",
+                style = "Gaya",
+                color = "Warna",
+                highlight = "Highlight",
+                texture = "Tekstur",
+                fade = "Memudar"
+            },
+            opacity = "Kegelapan",
+            style = "Gaya",
+            color = "Warna",
+            secondColor = "Warna Sekunder",
+            blemishes = "Noda",
+            beard = "Jenggot",
+            eyebrows = "Alis",
+            ageing = "Penuaan",
+            makeUp = "Make up",
+            blush = "Kemerahan Pipi",
+            complexion = "Corak",
+            sunDamage = "Kersakan Muka Akibat Sinar Matahari",
+            lipstick = "Lipstik",
+            moleAndFreckles = "Tahi Lalat dan Bintik-bintik",
+            chestHair = "Bulu dada",
+            bodyBlemishes = "Noda tubuh",
+            eyeColor = "Warna mata"
+        },
+        components = {
+            title = "Pakaian",
+            drawable = "Pilihan",
+            texture = "Tekstur Baju",
+            mask = "Masker",
+            upperBody = "Lengan",
+            lowerBody = "Celana",
+            bags = "Tas Dan Parasut",
+            shoes = "Sepatu",
+            scarfAndChains = "Syal Dan Kalung",
+            shirt = "Baju",
+            bodyArmor = "Bodi Armor",
+            decals = "Stiker",
+            jackets = "Jaket",
+            head = "Topi"
+        },
+        props = {
+            title = "Atribut",
+            drawable = "Pilihan",
+            texture = "Tekstur",
+            hats = "Topi Dan Helm",
+            glasses = "Kacamata",
+            ear = "Aksesoris Telinga",
+            watches = "Jam",
+            bracelets = "Gelang"
+        },
+        tattoos = {
+            title = "Tato",
+            items = {
+                ZONE_TORSO = "Badan",
+                ZONE_HEAD = "Kepala",
+                ZONE_LEFT_ARM = "Tangan Kiri",
+                ZONE_RIGHT_ARM = "Tangan Kanan",
+                ZONE_LEFT_LEG = "Kaki Kiri",
+                ZONE_RIGHT_LEG = "Kaki Kanan"
+            },
+            apply = "Terapkan",
+            delete = "Hapus",
+            deleteAll = "Hapus Semua Tato",
+            opacity = "Kegelapan"
+        }
+    },
+    outfitManagement = {
+        title = "Pakaian Pekerjaan",
+        jobText = "Atur pakaian untuk pekerja",
+        gangText = "Atur pakaian untuk anggota gang"
+    },
+    cancelled = {
+        title = "Kostumisasi di batalkan",
+        description = "Kustomisasi tidak disimpan"
+    },
+    outfits = {
+        import = {
+            title = "Mauskkan kode pakaian",
+            menuTitle = "Impor Pakaian",
+            description = "Impor pakaian dari kode yang di bagikan",
+            name = {
+                label = "Nama Pakaian",
+                placeholder = "Pakaian yang bagus",
+                default = "Pakaian Impor"
+            },
+            code = {
+                label = "Kode Pakaian"
+            },
+            success = {
+                title = "Berhasil",
+                description = "Anda sekarang dapat mengganti pakaian menggunakan menu pakaian"
+            },
+            failure = {
+                title = "Gagal",
+                description = "Kode pakaian tidak valid"
+            }
+        },
+        generate = {
+            title = "Lihat Kode Pakaian",
+            description = "Lihat kode pakaian untuk di bagikan.",
+            failure = {
+                title = "Ada yang salah",
+                description = "Pembuatan kode gagal untuk pakaian tersebut"
+            },
+            success = {
+                title = "Kode Pakaian Dihasilkan",
+                description = "Ini kode pakaianmu"
+            }
+        },
+        save = {
+            menuTitle = "Simpan Pakaian saat ini",
+            menuDescription = "Simpan pakaian Anda saat ini sebagai pakaian %s",
+            description = "Simpan pakaian Anda saat ini",
+            title = "Beri nama pakaian Anda",
+            managementTitle = "Detail Pakaian Pekerjaan",
+            name = {
+                label = "Nama Pakaian",
+                placeholder = "Pakaian yang bagus"
+            },
+            gender = {
+                label = "Jenis Kelamin",
+                male = "Laki-Laki",
+                female = "Perempuan"
+            },
+            rank = {
+                label = "Pangkat Minimal"
+            },
+            failure = {
+                title = "Gagal Menyimpan",
+                description = "Pakaian dengan nama ini sudah ada"
+            },
+            success = {
+                title = "Berhasil Menyimpan",
+                description = "Pakaian dengan nama %s berhasil di simpan"
+            }
+        },
+        update = {
+            title = "Perbarui Pakaian",
+            description = "Simpan pakaian Anda saat ini ke pakaian yang sudah ada",
+            failure = {
+                title = "Gagal Memperbarui",
+                description = "Pakaian itu tidak ada"
+            },
+            success = {
+                title = "Berhasil Memperbarui",
+                description = "Pakaian dengan nama %s telah di perbarui"
+            }
+        },
+        change = {
+            title = "Ganti Pakaian",
+            description = "Pilih dari %s pakaian yang Anda simpan saat ini",
+            pDescription = "Pilih dari salah satu pakaian yang Anda simpan saat ini",
+            failure = {
+                title = "Ada yang salah",
+                description = "Pakaian yang ingin Anda ganti tidak memiliki tampilan dasar",
+            }
+        },
+        delete = {
+            title = "Hapus Pakaian",
+            description = "Hapus pakaian %s yang disimpan",
+            mDescription = "Hapus semua pakaian yang Anda simpan",
+            item = {
+                title = 'Hapus "%s"',
+                description = "Model: %s%s"
+            },
+            success = {
+                title = "Berhasil",
+                description = "Pakaian di hapus"
+            }
+        },
+        manage = {
+            title = "👔 | Atur %s Pakaian"
+        }
+    },
+    jobOutfits = {
+        title = "Pakaian Pekerjaan",
+        description = "Pilih dari salah satu pakaian kerja Anda"
+    },
+    menu = {
+        returnTitle = "Kembali",
+        title = "Ruang Pakaian",
+        outfitsTitle = "Menu Pakaian",
+        clothingShopTitle = "Toko Pakaian",
+        barberShopTitle = "Toko Rambut",
+        tattooShopTitle = "Toko Tato",
+        surgeonShopTitle = "Operasi Plastik"
+    },
+    clothing = {
+        title = "Beli Pakaian - $%d",
+        titleNoPrice = "Ganti Pakaian",
+        options = {
+            title = "👔 | Pilihan Toko Pakaian",
+            description = "Pilih dari beragam item untuk dikenakan"
+        },
+        outfits = {
+            title = "👔 | Pilihan Pakaian",
+            civilian = {
+                title = "Pakaian Sipil",
+                description = "Pakai baju mu"
+            }
+        }
+    },
+    commands = {
+        reloadskin = {
+            title = "Muat ulang karakter Anda",
+            failure = {
+                title = "Error",
+                description = "Anda tidak dapat menggunakan reloadskin saat ini"
+            }
+        },
+        clearstuckprops = {
+            title = "Menghapus semua props yang melekat pada anda",
+            failure = {
+                title = "Error",
+                description = "Anda tidak dapat menggunakan clearstuckprops saat ini"
+            }
+        },
+        pedmenu = {
+            title = "Buka / Berikan Menu Pakaian",
+            failure = {
+                title = "Error",
+                description = "Pemain tidak online"
+            }
+        },
+        joboutfits = {
+            title = "Buka Menu Pakaian Pekerjaan"
+        },
+        gangoutfits = {
+            title = "Buka Menu Pakaian Gang"
+        },
+        bossmanagedoutfits = {
+            title = "Membuka Menu Pakaian yang Dikelola Bos"
+        }
+    },
+    textUI = {
+        clothing = "Tokok Pakaian - Price: $%d",
+        barber = "Salon - Price: $%d",
+        tattoo = "Tato - Price: $%d",
+        surgeon = "Operasi Plastik - Price: $%d",
+        clothingRoom = "Ruang Pakaian",
+        playerOutfitRoom = "Pakaian"
+    },
+    migrate = {
+        success = {
+            title = "Berhasil",
+            description = "Migrasi selesai. %s kulit bermigrasi",
+            descriptionSingle = "Kulit yang Bermigrasi"
+        },
+        skip = {
+            title = "Informasi",
+            description = "Melewatkan kulit"
+        },
+        typeError = {
+            title = "Error",
+            description = "Jenis tidak valid"
+        }
+    },
+    purchase = {
+        tattoo = {
+            success = {
+                title = "Berhasil",
+                description = "Membuat %s tato dengan harga %s$"
+            },
+            failure = {
+                title = "Penerapan Tato Gagal",
+                description = "Kamu tidak memiliki cukup uang!"
+            }
+        },
+        store = {
+            success = {
+                title = "Berhasil",
+                description = "Telah memberi $%s ke %s!"
+            },
+            failure = {
+                title = "Exploit!",
+                description = "Anda tidak punya cukup uang! Mencoba mengeksploitasi sistem!"
+            }
+        }
+    }
+}
